93bc90b80d9386ee0144a7f1d9ef894cdecfd979
TakumiHiguchi <andf.for.gt@gmail.com>
2020-06-29 15:46:52 +0900

    [change] CORSの変更

--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,7 @@ module NurtureAPI
     # Permit cross origin
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins "localhost:3000", "http://java.cse.ce.nihon-u.ac.jp"
+        origins "java.cse.ce.nihon-u.ac.jp", "localhost:3000" , "http://java.cse.ce.nihon-u.ac.jp",
         resource "*",
           headers: :any,
           methods: [:get, :post, :options, :head]
