a13b77bafd39d0735baed4b419de6aefd29559e4
TakumiHiguchi <andf.for.gt@gmail.com>
2020-07-07 17:29:05 +0900

    [add] task取得APIを作成した

--- app/controllers/api/v1/task_controller.rb
+++ app/controllers/api/v1/task_controller.rb
@@ -1,15 +1,75 @@
 class Api::V1::TaskController < ApplicationController
     
+    before_action :userSignedin?, only: [:index,:create,:update] #セッションの確認
+    
     def index
+        if @userSession
+            #sessionが有効だったらユーザーのタスクを返す
+            task = UserTask.where(user_id:@user.id)
+            result = {}
+            
+            task.each do |t|
+                #年:月:日をkeyに持つhashを生成し、そこにタスク内容をpushしていく
+                #年の作成
+                reIns = result[t.taskDate.strftime("%Y").to_i]
+                if reIns.nil?
+                    result[t.taskDate.strftime("%Y").to_i] ={}
+                    reIns = result[t.taskDate.strftime("%Y").to_i]
+                end
+                #月の作成
+                reIns1 = reIns[t.taskDate.strftime("%m").to_i]
+                if reIns1.nil?
+                    result[t.taskDate.strftime("%Y").to_i][t.taskDate.strftime("%m").to_i] = {}
+                    reIns1 = result[t.taskDate.strftime("%Y").to_i][t.taskDate.strftime("%m").to_i]
+                end
+                #日の作成
+                reIns2 = reIns1[t.taskDate.strftime("%d").to_i]
+                if reIns2.nil?
+                    result[t.taskDate.strftime("%Y").to_i][t.taskDate.strftime("%m").to_i][t.taskDate.strftime("%d").to_i] = []
+                    reIns2 = result[t.taskDate.strftime("%Y").to_i][t.taskDate.strftime("%m").to_i][t.taskDate.strftime("%d").to_i]
+                end
+
+                
+                ins = {
+                    title:t.title,
+                    content:t.content,
+                    date:t.taskDate,
+                    position:t.taskDate
+                }
+                reIns2.push(ins)
+                result[t.taskDate.strftime("%Y").to_i][t.taskDate.strftime("%m").to_i][t.taskDate.strftime("%d").to_i] = reIns2
+            end
+            if result
+                mes = "タスクを取得しました"
+            else
+                mes = "タスクの取得に失敗しました。"
+            end
+        else
+            result = nil
+            mes = "セッションが無効です"
+        end
+        
+        if result
+            render json: JSON.pretty_generate({
+                                              status:'SUCCESS',
+                                              api_version: 'v1',
+                                              mes: mes,
+                                              tasks:result
+            })
+        else
+            render json: JSON.pretty_generate({
+                                              status:'ERROR',
+                                              api_version: 'v1',
+                                              mes: mes
+            })
+        end
+        
+        
     end
     def create
-        #sessionの確認
-        user = User.find_by(key: params[:key],session: params[:session])
-        userSession = user.maxAge.to_i > Time.now.to_i if user
-        
-        if userSession
-            #sessionが有効だったらユーザーのスケジュールを返す
-            result = UserTask.create(user_id:user.id, title: params[:title], content: params[:content], taskDate:params[:taskdate], position:params[:position])
+        if @userSession
+            #sessionが有効だったらタスクを作る
+            result = UserTask.create(user_id:@user.id, title: params[:title], content: params[:content], taskDate:params[:taskdate], position:params[:position])
             if result
                 mes = "タスクを作成しました"
             else
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+    
+    def userSignedin?
+        #sessionの確認
+        @user = User.find_by(key: params[:key],session: params[:session])
+        @userSession = @user.maxAge.to_i > Time.now.to_i if @user
+    end
 end
