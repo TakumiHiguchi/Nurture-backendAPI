d0376bcbc1d3a894c3336bc90083f550100ca1ab
TakumiHiguchi <andf.for.gt@gmail.com>
2020-07-28 20:19:50 +0900

    [add] カレンダーを削除するAPiの追加

--- app/controllers/api/v1/calendar_controller.rb
+++ app/controllers/api/v1/calendar_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::CalendarController < ApplicationController
-    before_action :userSignedin?, only: [:index,:update,:destroy] #セッションの確認
+    before_action :userSignedin?, only: [:index,:create,:update,:destroy] #セッションの確認
     before_action :calendarOwn?, only: [:update,:destroy] #カレンダーの所有者か確認
     def index
         if @userSession
@@ -60,16 +60,53 @@ class Api::V1::CalendarController < ApplicationController
             })
         end
     end
-    def update
-        if @userSession && @calendarOwn
-            calendar = Calendar.find_by(id:params[:calendarId])
-            calBoolean = calendar.update(
+    def create
+        if @userSession
+            o = [('a'..'z'), ('A'..'Z'), ('0'..'9')].map { |i| i.to_a }.flatten
+            key = (0...20).map { o[rand(o.length)] }.join
+            result = Calendar.create(
+                key:key,
                 name: params[:name],
                 description: params[:description],
-                color: params[:color],
                 shareBool: params[:shareBool],
                 cloneBool: params[:cloneBool],
+                author_id: @user.id
             )
+            if result then UserCalendarRelation.create(user_id:@user.id, calendar_id:result.id) end
+        else
+            calBoolean = false
+            mes = "セッション切れです"
+        end
+        if result
+            render json: JSON.pretty_generate({
+                                              status:'SUCCESS',
+                                              api_version: 'v1',
+                                              mes: "カレンダーを作成しました。"
+            })
+        else
+            render json: JSON.pretty_generate({
+                                              status:'ERROR',
+                                              api_version: 'v1',
+                                              mes: mes
+            })
+        end
+    end
+    def update
+        if @userSession && @calendarOwn
+            
+            if params[:name] == "" 
+                calBoolean = false
+                mes = "名前は最低1文字以上でなくてはなりません"
+            else
+                calendar = Calendar.find_by(id:params[:calendarId])
+                calBoolean = calendar.update(
+                    name: params[:name],
+                    description: params[:description],
+                    color: params[:color],
+                    shareBool: params[:shareBool],
+                    cloneBool: params[:cloneBool]
+                )
+            end
         else
             calBoolean = false
             mes = "アクセス権限がありません。"
@@ -81,9 +118,6 @@ class Api::V1::CalendarController < ApplicationController
                                               mes: "カレンダーを変更しました。"
             })
         else
-            if name == "" 
-                mes = "名前は最低1文字以上でなくてはなりません"
-            end
             render json: JSON.pretty_generate({
                                               status:'ERROR',
                                               api_version: 'v1',
