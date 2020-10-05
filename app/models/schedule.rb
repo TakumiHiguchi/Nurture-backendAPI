7a62694253d6f38634778426721dfe73b9f7b8d9
TakumiHiguchi <andf.for.gt@gmail.com>
2020-10-06 01:53:00 +0900

    ref: schedule_modelのインデントを直した

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -50,20 +50,18 @@ class Schedule < ApplicationRecord
   end
 
   def self.searchAPI(type,params)
-      return self.all unless params.to_s.length > 0
+    return self.all unless params.to_s.length > 0
 
-      case type
-      when "fr" then
-          keywords = params.split(/[[:blank:]]+/)
-          schedules = self
-          
-          keywords.each do |keyword|
-              schedules = schedules.where("UPPER(title) LIKE ?", "%#{keyword.upcase}%") #分割されたキーワードでの検索
-          end
-          
-          schedules.uniq
-      when "position" then
-          self.where(:title => params)
+    case type
+    when "fr" then
+      keywords = params.split(/[[:blank:]]+/)
+      schedules = self
+      keywords.each do |keyword|
+        schedules = schedules.where("UPPER(title) LIKE ?", "%#{keyword.upcase}%") #分割されたキーワードでの検索
       end
+      schedules.uniq
+    when "position" then
+      self.where(:title => params)
+    end
   end
 end
