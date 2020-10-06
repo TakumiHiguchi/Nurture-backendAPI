156fec2f506c60c68fd8a081eae228a839d7c0e5
TakumiHiguchi <andf.for.gt@gmail.com>
2020-10-06 14:10:07 +0900

    ref: UserDetailモデルにスキーマを追加

--- app/models/user_detail.rb
+++ app/models/user_detail.rb
@@ -1,4 +1,11 @@
 class UserDetail < ApplicationRecord
+  # スキーマ
+  # user_id(integer)
+  # name(string)
+  # grade(integer)
+  # created_at(datetime)  :precision => 6, :null => false
+  # updated_at(datetime)  :precision => 6, :null => false
+
   #バリデーション
   validates :name,    :presence => true
   validates :grade,   :presence => true
