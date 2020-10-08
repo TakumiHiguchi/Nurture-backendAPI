4c77397d9780237d15e1246b59d7b0283892c8e9
TakumiHiguchi <andf.for.gt@gmail.com>
2020-10-08 14:44:38 +0900

    add:  post /api/v1/calendarのAPIテストを書いた

--- spec/requests/api/v1/calendar_spec.rb
+++ spec/requests/api/v1/calendar_spec.rb
@@ -46,6 +46,26 @@ describe 'calendarAPI' do
 			end
 		end
 	end
+
+	describe 'post /api/v1/calendar' do
+		context 'サインインしている時' do
+			before do
+				# keyとsessionを与えてログイン状態にする
+				calendar_hash[:key] = create_user.key
+				calendar_hash[:session] = create_user.session
+			end
+			it 'カレンダーが作成されること' do
+				expect { post '/api/v1/calendar', params: calendar_hash }.to change(Calendar, :count).by(1)
+				expect(response.status).to eq(200)
+			end
+		end
+		context 'サインインしていない時' do
+			it '401ステータスが帰ってくること' do
+				post '/api/v1/calendar'
+				expect(response.status).to eq(401)
+			end
+		end
+	end
 end
 
 
