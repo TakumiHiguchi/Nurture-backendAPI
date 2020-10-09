f9d81b0d8f00b30971a7e74f5e34a1d020cd53de
TakumiHiguchi <andf.for.gt@gmail.com>
2020-10-09 21:44:28 +0900

    add: calendar_shareAPIのcloneメソッドのテストを書いた

--- spec/requests/api/v1/calendar_share_spec.rb
+++ spec/requests/api/v1/calendar_share_spec.rb
@@ -63,6 +63,50 @@ describe 'calendar_shareAPI' do
 			end
 		end
 	end
+
+	describe 'post /api/v1/calendar_clone' do
+		context 'サインインしている時' do
+			before do
+				@params = {
+					:calendarId => create_calendar.id,
+					:key => create_calendar.users.first.key,
+					:session => create_calendar.users.first.session
+				}
+			end
+			it 'カレンダーが一つ作成されていること' do
+				expect { post '/api/v1/calendar_clone', params: @params }.to change(Calendar, :count).by(1)
+			end
+			it 'カレンダーが複製されること' do
+				source = Calendar.find(@params[:calendarId])
+
+				# 正しい個数が生成されているかの確認
+				expect( Calendar.last.tasks.count ).to eq( create_calendar.tasks.count )
+				expect( Calendar.last.exams.count ).to eq( create_calendar.exams.count )
+				expect( Calendar.last.change_schedules.count ).to eq( create_calendar.change_schedules.count )
+				expect( Calendar.last.calendar_schedule_relations.count ).to eq( create_calendar.calendar_schedule_relations.count )
+				expect( Calendar.last.semester_periods.count ).to eq( create_calendar.semester_periods.count )
+				expect( Calendar.last.transfer_schedules.count ).to eq( create_calendar.transfer_schedules.count )
+
+				# 正しく複製されているかの確認
+				expect( Calendar.last.tasks.first ).to eq( create_calendar.tasks.first )
+				expect( Calendar.last.exams.first ).to eq( create_calendar.exams.first )
+				expect( Calendar.last.change_schedules.first ).to eq( create_calendar.change_schedules.first )
+				expect( Calendar.last.calendar_schedule_relations.first ).to eq( create_calendar.calendar_schedule_relations.first )
+				expect( Calendar.last.semester_periods.first ).to eq( create_calendar.semester_periods.first )
+				expect( Calendar.last.transfer_schedules.first ).to eq( create_calendar.transfer_schedules.first )
+			end
+			it '200ステータスが帰ってくること' do
+				post '/api/v1/calendar_clone', params: @params
+				expect(response.status).to eq(200)
+			end
+		end
+		context 'サインインしていない時' do
+			it '401ステータスが帰ってくること' do
+				post '/api/v1/calendar'
+				expect(response.status).to eq(401)
+			end
+		end
+	end
 end
 
 
